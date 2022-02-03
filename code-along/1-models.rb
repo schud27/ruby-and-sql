a264bcc8c7575f5725f0856eff5a6b1b58c41ef1
schud27 <dan.schulte@kellogg.northwestern.edu>
2022-02-03 01:52:10 +0000

    added companies

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -11,9 +11,48 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+puts "There are #{Company.all.count} companies."
+
 # 2. create new companies
 
+values = {
+    name: "Apple", 
+    url: "https://apple.com", 
+    city: "Cupertino", 
+    state: "CA"
+}
+company = Company.new(values)
+company.save
+
+puts "There are #{Company.all.count} companies."
+
+values = {
+    name: "Amazon", 
+    url: "https://amazon.com", 
+    city: "Seattle", 
+    state: "WA"
+}
+company = Company.new(values)
+company.save
+
+puts "There are #{Company.all.count} companies."
+
+
+company = Company.new
+# company.write_attribute(:name, "Tesla")
+company.name = "Tesla"
+
+puts "There are #{Company.all.count} companies."
+
 # 3. query companies table
+
+
+puts Company.all.inspect
+companies = Company.where({state: "CA",})
+puts companies.inspect
+
+apple = Company.where({state: "CA", name: "Apple"})[0]
+puts companies.inspect
 
 # 4. read column values from row
 
