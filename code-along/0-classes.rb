8e99c309de797239466e2e5470111a969c2f6a73
schud27 <dan.schulte@kellogg.northwestern.edu>
2022-02-03 00:58:16 +0000

    updated classes

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -4,7 +4,24 @@
 # rails runner code-along/0-classes.rb
 
 # String Class
+bens_favorite_food = "sushi"
+puts bens_favorite_food
+puts bens_favorite_food.class
+
+brians_favorite_food = "tacos"
+puts brians_favorite_food.upcase
+
+number_of_tacos = 5
+puts number_of_tacos.class
 
 # String Methods
 
 # Custom Class
+class Dog
+    def speak
+        puts "Woof"
+    end
+end
+
+jenkins = Dog.new
+puts jenkins.speak
